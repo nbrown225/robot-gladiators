168cdcb3e2bdfabe4c912dcba892bf63839e38c9
nbrown225 <chbrown225@yahoo.com>
2021-12-13 19:51:08 -0800

    added changes

---
+++ notes.js
@@ -0,0 +1,17 @@
+// ASKS USER FOR INPUT. DATA CAN BE STORED IN BROWSER //
+var playerName = window.prompt("WHAT DO YOU CALL YOUR ROBOT???")
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("OUR ROBOT'S NAME IS " + playerName)
+
+// this names the function //
+function fight() {
+window.alert("THE FIGHT HAS BEGUN!");
+}
+//this calls the function we named //
+// fight();
+// //
+
+var cars = ["toyota", "cheve", "ford", "dodge"]
+function cars() 
